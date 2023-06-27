759bbad56e984117442f6a712bf56ac0fda6b99c
jhsong76 <20190976@sungshin.ac.kr>
2023-06-27 16:30:17 +0900

    [INIT] 프로젝트 초기세팅

---
+++ src/main/java/com/example/mindfriend/MindfriendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mindfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindfriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindfriendApplication.class, args);
+	}
+
+}
