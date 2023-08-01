f28b1835005a6afd151592dc2923e8123ad5976c
jhsong76 <20190976@sungshin.ac.kr>
2023-08-01 12:25:17 +0900

    [FEAT] 로그인 api #7

--- src/main/java/com/example/mindfriend/security/CustomUserDetailService.java
+++ src/main/java/com/example/mindfriend/security/CustomUserDetailService.java
@@ -6,12 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
@@ -22,7 +24,7 @@ public class CustomUserDetailService implements UserDetailsService {
 
     // 해당하는 User의 데이터가 존재한다면 UserDetails 객체로 만들어서 리턴
     private UserDetails createUserDetails(User user) {
-        return User.builder()
+        return (UserDetails) User.builder()
                 .userId(user.getUserId())
                 .userPassword(user.getUserPassword())
                 .build();
--- src/main/java/com/example/mindfriend/service/AuthService.java
+++ src/main/java/com/example/mindfriend/service/AuthService.java
@@ -2,10 +2,16 @@ package com.example.mindfriend.service;
 
 import com.example.mindfriend.common.response.exception.MindFriendBusinessException;
 import com.example.mindfriend.domain.User;
+import com.example.mindfriend.dto.request.signIn;
 import com.example.mindfriend.dto.request.signUp;
 import com.example.mindfriend.dto.response.*;
 import com.example.mindfriend.repository.UserRepository;
+import com.example.mindfriend.security.JwtTokenProvider;
+import com.example.mindfriend.security.TokenInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +24,8 @@ import static com.example.mindfriend.common.response.exception.ErrorCode.*;
 public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     // 회원가입
     public getUser signUp(signUp request) {
@@ -34,5 +42,21 @@ public class AuthService {
         userRepository.save(user);
 
         return getUser.of(user);
+    }
+
+    // 로그인
+    public TokenInfo signIn(signIn request) {
+        // 1. Login ID/PW 기반으로 Authentication 객체 생성
+        // 이때 authentication는 인증 여부를 확인하는 authenticated 값이 false
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(request.getUserId(), request.getUserPassword());
+
+        // 2. 실제 검증 (사용자 비밀번호 체크)이 이루어지는 부분
+        // authenticate 메서드가 실행될 때 CustomUserDetailService에서 만든 loadUserByUsername 메서드가 실행
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 3. 인증 정보를 기반으로 JWT 토큰 생성
+        TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
+
+        return tokenInfo;
     }
 }
