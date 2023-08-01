45ead655644d2d32e854c5bc28087d811376becf
jhsong76 <20190976@sungshin.ac.kr>
2023-08-01 15:09:04 +0900

    [FIX] URL 수정 #7

--- src/main/java/com/example/mindfriend/controller/UserController.java
+++ src/main/java/com/example/mindfriend/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
     private final UserService userService;
     private final SecurityUtils securityUtils;
     // 유저 단일 조회
-    @GetMapping("/")
+    @GetMapping("/read")
     public ResultResponse<getUser> getUser() {
         getUser response = userService.getUser(securityUtils.getCurrentUserId());
         return new ResultResponse<>(GET_USER_SUCCESS, response);
