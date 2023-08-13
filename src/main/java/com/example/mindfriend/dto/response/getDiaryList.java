c11f27fe669d0f5b6606246029fc397fb6a4e44f
jhsong76 <20190976@sungshin.ac.kr>
2023-08-13 12:50:36 +0900

    [FIX] 일기 리스트 조회시 response값 수정 #23

--- src/main/java/com/example/mindfriend/dto/response/getDiaryList.java
+++ src/main/java/com/example/mindfriend/dto/response/getDiaryList.java
@@ -3,6 +3,7 @@ package com.example.mindfriend.dto.response;
 import com.example.mindfriend.domain.Diary;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,10 +13,14 @@ import java.util.stream.Collectors;
 @Builder
 public class getDiaryList {
     private Long diaryIdx;
+    private LocalDateTime date;
 
     public static List<getDiaryList> of(List<Diary> diaries) {
         return diaries.stream()
-                .map(diary -> getDiaryList.builder().diaryIdx(diary.getDiaryIdx()).build())
+                .map(diary -> getDiaryList.builder()
+                        .diaryIdx(diary.getDiaryIdx())
+                        .date(diary.getCreatedAt())
+                        .build())
                 .collect(Collectors.toList());
     }
 }
