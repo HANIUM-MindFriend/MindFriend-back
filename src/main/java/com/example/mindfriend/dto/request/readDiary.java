504cc55db68aca6b7400387cf439d1ef2eb4795b
elive7 <parksub020704@gmail.com>
2023-08-16 20:01:54 +0900

    {FIX} 일기 조회 api 수정 완료

---
+++ src/main/java/com/example/mindfriend/dto/request/readDiary.java
@@ -0,0 +1,10 @@
+package com.example.mindfriend.dto.request;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class readDiary {
+    private Date date;
+}
--- src/main/java/com/example/mindfriend/security/SecurityUtils.java
+++ src/main/java/com/example/mindfriend/security/SecurityUtils.java
@@ -20,4 +20,6 @@ public class SecurityUtils {
         }
         return authentication.getName();
     }
+
+
 }
