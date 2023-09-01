018a1ed609db50f88ae207d3f1284978155f793b
SONG JU HEE <80883709+jhsong76@users.noreply.github.com>
2023-09-01 11:07:10 +0900

    Merge pull request #26 from HANIUM-MindFriend/feature#21
    Feature#21 AI 서버 통신

--- src/main/java/com/example/mindfriend/service/DiaryService.java
+++ src/main/java/com/example/mindfriend/service/DiaryService.java
@@ -18,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
