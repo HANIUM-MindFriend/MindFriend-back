a95529c20354226da2e497fe7b1064b36f69a4a0
jhsong76 <20190976@sungshin.ac.kr>
2023-09-03 01:15:42 +0900

    [FEAT] 일기 저장 시 전체 문장 저장 #27

--- src/main/java/com/example/mindfriend/repository/DiaryRepository.java
+++ src/main/java/com/example/mindfriend/repository/DiaryRepository.java
@@ -3,6 +3,8 @@ package com.example.mindfriend.repository;
 import com.example.mindfriend.domain.Diary;
 import com.example.mindfriend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,6 +15,10 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     List<Diary> findByCreatedAtBetweenAndMainEmotion(LocalDateTime startDateTime, LocalDateTime endDateTime, Long emotion);
 
-    List<Diary> findByCreatedAtBetweenAndContentContaining(LocalDateTime startDateTime, LocalDateTime endDateTime,String keyword);
+    List<Diary> findByCreatedAtBetweenAndContentContaining(LocalDateTime startDateTime, LocalDateTime endDateTime, String keyword);
+
     List<Diary> findByCreatedAtBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
+    @Query("SELECT d FROM Diary d WHERE d.user = :user AND FUNCTION('DATE', d.createdAt) = FUNCTION('DATE', :today)")
+    Diary findDiariesCreatedToday(@Param("user") User user, @Param("today") LocalDateTime today);
+
 }
