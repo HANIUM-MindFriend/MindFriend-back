00660d3aaef1f9eb64b0750925ec7a2d3d672bac
jhsong76 <20190976@sungshin.ac.kr>
2023-09-13 03:00:24 +0900

    [FIX] 파이썬 버전 변경 #9

--- src/main/java/com/example/mindfriend/script/run_python_script.sh
+++ src/main/java/com/example/mindfriend/script/run_python_script.sh
@@ -1,7 +1,7 @@
-#!/bin/bash리
+#!/bin/bash
 
 #Python 파일 실행
-python /home/ubuntu/app/ai/main.py
+python3 /home/ubuntu/app/ai/main.py
 
 # Python 스크립트의 결과를 스프링 부트로 반환
-echo "Python script executed successfully!"
+echo "Python3 script executed successfully!"
